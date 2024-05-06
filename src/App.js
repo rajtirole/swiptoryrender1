2b81e03740eeca71c2109a018041b8ffdab30120
rajtirole <rtech23456@gmail.com>
2024-05-06 11:25:55 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { Routes ,Route} from 'react-router-dom';
+import { Routes ,Route, useNavigate} from 'react-router-dom';
 
 function App() {
+  const navigate=useNavigate()
   return (
     <BrowserRouter>
     <Routes>
-    <Route path="/" element={<div>homepage</div>} />
+    <Route path="/" element={<div>homepage
+      <button onClick={()=>{
+        navigate('/test')
+      }}>test</button>
+    </div>} />
     <Route path="/story" element={<div>story</div>} />
     <Route path="/test" element={<div>test</div>} />
     <Route path="/getId/:id" element={<div>get by id </div>} />
